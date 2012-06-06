b1be1a3576f204144e3066115249fb8a32e7e250
Udo Schroeter <ec2-user@ip-10-116-62-172.ec2.internal>
2012-06-06 12:55:44 +0000

    fixed jquery fileupload problem

--- conf/default.php
+++ conf/default.php
@@ -1,14 +1,14 @@
 <?php $GLOBALS['config'] = array(
   'db' => array(
     "host" => "localhost",
-    "user" => "mysql_user",
-    "password" => "mysql_password",
-    "database" => "mysql_db",
+    "user" => "adhocdemo",
+    "password" => "Znr2fmU4vHPMWV7u",
+    "database" => "adhocdemo",
     "prefix" => "ah_"
   ),
   'service' => array(
-    "server" => "adhocistan.com",
-    "adminpw" => "N2M4Yzdk",
+    "server" => "gapmind.com",
+    "adminpw" => "349thsflk",
     "url_rewrite" => true,
     "salt" => "i6r3bkcbyfr442zo"
 )); ?>
